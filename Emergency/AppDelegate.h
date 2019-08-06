4da37c2708dc970875be9ae15f41a117efd9faee
古智性 <zhixing.gu@qianli-inc.com>
2019-08-06 21:32:36 +0800

    Initial Commit

---
+++ Emergency/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Emergency
+//
+//  Created by gzx on 2019/8/6.
+//  Copyright © 2019 古智性. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Emergency/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Emergency
+//
+//  Created by gzx on 2019/8/6.
+//  Copyright © 2019 古智性. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
